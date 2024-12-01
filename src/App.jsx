7564eeea19de1ed1e3fe4fee16e84e8071bb8137
Phrdavis <davipi_sou@hotmail.com>
2024-12-01 12:19:59 -0300

    Função de verificação da letra adicionando as tentativas as suas respectivas listas

--- src/App.jsx
+++ src/App.jsx
@@ -64,8 +64,23 @@ function App() {
 
   const verifyLetter = (letter) =>{
 
-    // setGameStage(stages[2].name)
-    console.log(letter)
+    const normalizedLetter = letter.toLowerCase();
+
+    if(guessedLetters.includes(normalizedLetter) || wrongLetters.includes(normalizedLetter)){
+
+      return;
+
+    } 
+
+    if(letters.includes(normalizedLetter)){
+
+      setGuessedLetters([...guessedLetters, normalizedLetter]);
+
+    }else{
+
+      setWrongLetters([...wrongLetters, normalizedLetter]);
+
+    }
 
   }
 
